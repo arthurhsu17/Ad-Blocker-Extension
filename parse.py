afb07750b5117b8432dd626f5162d8edbb94f1db
Arthur Hsu <71420919+arthurhsu17@users.noreply.github.com>
2022-06-30 00:41:44 +0800

    Add files via upload

--- parse.py
+++ parse.py
@@ -6,15 +6,15 @@ file_output.write("var blocked_sites = [\n")
 
 initial = True
 while(line):
-    if("127.0.0.1 " not in line): 
+    if("127.0.0.1 " in line): 
         if not initial:
             file_output.write(",\n")
         initial= False
         line = (line.lstrip("127.0.0.1 ")).rstrip("]\n")
         new_line = "*://*." + line + "/*"
-        file_output.write("\""+ new_line + "\"")
+        file_output.write("\"" + new_line + "\"")
     line = file_input.readline()
-
+ 
 file_output.write("]")
 file_output.close()
 file_input.close()
